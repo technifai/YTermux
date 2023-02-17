314adf3f812cb3e951712ec322d9b37f00d46890
technifai <125653653+technifai@users.noreply.github.com>
2023-02-17 16:01:42 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# YTermux
+# Made by: tabeer Shahjahan
+# Instagram: @technifai
+# Github: https://github.com/technifai
+
+
+#Probably make a varible with the file name 
+#make it more easy to update!
+echo -e "\e[035m"  "Updating default packages\n"
+apt update && apt upgrade -y
+
+echo -e "\e[032m" "Requesting acces to storage\n"
+echo -e "\e[032m" "Allow Storage Permission!"
+sleep 2
+termux-setup-storage 
+
+#echo -e "\e[033m" "Installing python\n"
+pkg install python -y
+
+echo -e "\e[034m"  "Installing yt-dlp\n"
+pip install yt-dlp
+
+echo -e "\e[032m"  "Making the Youtube Directory to download the Vidoes\n"
+mkdir ~/storage/shared/Youtube
+
+
+echo -e "Creating bin folder\n"
+mkdir ~/bin
+
+echo -e "Creating Termux-URL-Opener Script.\n"
+
+mv termux-url-opener ~/bin/
+
+chmod +x ~/bin/termux-url-opener
+
+echo -e "\n"
+echo -e "\e[032m" "Process Complete!"
+echo -e "\e[032m" "Now, share any Youtube video with Termux and you will be asked to select the quality of your video and after that,It will be automatically Downloaded. If You will share shorts, it will be download automatically."
+echo -e "\e[033m" "For More Awesome and Useful Tool like this follow @technifai on Instagram "
